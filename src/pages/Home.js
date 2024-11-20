c65540ab5dcc89f170e4af99e12454598d3a0e76
Daniel Penado <penadodan02@gmail.com>
2024-11-19 22:24:56 -0600

    Cards are links

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,16 +14,36 @@ const Home = () => {
       </header>
       <p className="subtitle">"Las flores hacen los momentos más especiales"</p>
       <section className="main-content">
-        <Card title="Ramos de Flores" imageSrc="/images/home/cards/Card.jpg"/>
-        <Card title="Arreglos Florales" imageSrc="/images/home/cards/Card.jpg"/>
-        <Card title="Flores Artificiales" imageSrc="/images/home/cards/Card.jpg"/>
-        <Card title="Bodas" imageSrc="/images/home/cards/Card.jpg"/>
-        <Card title="Graduaciones" imageSrc="/images/home/cards/Card.jpg"/>
-        <Card title="Funerales" imageSrc="/images/home/cards/Card.jpg"/>
-        <Card title="Aniversarios" imageSrc="/images/home/cards/Card.jpg"/>
-        <Card title="Decoración del hogar" imageSrc="/images/home/cards/Card.jpg"/>
-        <Card title="Empaques para regalos" imageSrc="/images/home/cards/Card.jpg"/>
+        <Link to="/" className="card-link">
+          <Card title="Ramos de Flores" imageSrc="/images/home/cards/Card.jpg"/>
+        </Link>
+        <Link to="/" className="card-link">
+          <Card title="Arreglos Florales" imageSrc="/images/home/cards/Card.jpg"/>
+        </Link>
+        <Link to="/" className="card-link">
+          <Card title="Flores Artificiales" imageSrc="/images/home/cards/Card.jpg"/>
+        </Link>
+        <Link to="/" className="card-link">
+          <Card title="Bodas" imageSrc="/images/home/cards/Card.jpg"/>
+        </Link>
+        <Link to="/" className="card-link">
+          <Card title="Graduaciones" imageSrc="/images/home/cards/Card.jpg"/>
+        </Link>
+        <Link to="/" className="card-link">
+          <Card title="Funerales" imageSrc="/images/home/cards/Card.jpg"/>
+        </Link>
+        <Link to="/" className="card-link">
+          <Card title="Aniversarios" imageSrc="/images/home/cards/Card.jpg"/>
+        </Link>
+        <Link to="/" className="card-link">
+          <Card title="Decoración del hogar" imageSrc="/images/home/cards/Card.jpg"/>
+        </Link>
+        <Link to="/" className="card-link">
+          <Card title="Empaques para regalos" imageSrc="/images/home/cards/Card.jpg"/>
+        </Link>
+        <Link to="/" className="card-link">
         <Card title="Otros" imageSrc="/images/home/cards/Card.jpg"/>
+        </Link>
       </section>
       <Footer />
     </div>
