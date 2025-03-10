d25e71502eeaffd7b551949462935ddc5acf2c26
Daniel Penado <penadodan02@gmail.com>
2025-03-09 22:42:09 -0600

    Opcion de pedido agregada

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import Navbar from '../components/navbar/Navbar';
 import Footer from '../components/footer/Footer';
 import { useParams } from 'react-router-dom';
@@ -10,22 +11,51 @@ const ProductDetail = () => {
   var productsInCategory = productsData[category]
   const product = productsInCategory.find(item => item.id === parseInt(id));
 
+  const [formData, setFormData] = useState({
+    name: "",
+    phone: "",
+    datetime: "",
+    details: "",
+    pickupDateTime: "",
+  });
+
   if (!product) {
     return <h2>Producto no encontrado</h2>;
   }
 
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Pedido realizado:", {
+      producto: product.name,
+      ...formData,
+    });
+    alert(`Pedido registrado correctamente. Nos pondremos en contacto contigo.`);
+  };
+
   return (
     <div>
     <Navbar />
-    <div className="product-detail-container">
-      <div className="product-image">
-        <img src={product.imageSrc} alt={product.name} />
-      </div>
+    <div className="product-detail">
+      <h1>{product.name}</h1>
+      <img src={product.imageSrc} alt={product.name} className="product-image" />
       <div className="product-info">
-        <h1>{product.name}</h1>
-        <p className="product-description">{product.description}</p>
-        <p className="product-price">{product.price}</p>
-        <button className="buy-button">Comprar Ahora</button>
+      <p className="product-description">{product.description}</p>
+        <p className="product-price">Precio: {product.price}</p>
+
+        <h3>¿Deseas pedirlo?</h3>
+        <form onSubmit={handleSubmit} className="order-form">
+          <input type="text" name="name" placeholder="Tu Nombre" value={formData.name} onChange={handleChange} required />
+          <input type="tel" name="phone" placeholder="Tu Teléfono" value={formData.phone} onChange={handleChange} required />
+          <textarea name="details" placeholder="Algun detalle que quieras" value={formData.details} onChange={handleChange} ></textarea>
+          <label>Fecha y Hora en el que lo necesitas:</label>
+          <input type="datetime-local" name="pickupDateTime" value={formData.pickupDateTime} onChange={handleChange} required />
+          <p className="important-note">📍 Los pedidos deben recogerse en nuestra floristería.</p>
+          <button type="submit">Enviar Pedido</button>
+        </form>
       </div>
     </div>
     <Footer />
